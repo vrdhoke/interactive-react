a062beda0a4ebf10b9d45c13a821025318d00328
Vaibhav Dhoke <vaibhavdhoke1@gmail.com>
2020-07-09 14:01:29 -0400

    Changes for deployment

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -6,8 +6,8 @@ export default function Roster() {
     return (
       <div>
         <Router>
-          <Route exact path='/' component={ListMovies}/>
-          <Route path='/:title/:date/:number' component={ListPeople}/>
+          <Route exact path='/interactive-react' component={ListMovies}/>
+          <Route path='/interactive-react/:title/:date/:number' component={ListPeople}/>
         </Router>
       </div>
     );
